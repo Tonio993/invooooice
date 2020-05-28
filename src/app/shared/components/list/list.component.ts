65a768d9a92b54d440dce399dc0e2e37d16a403a
Tonio993 <tonio.iuele@gmail.com>
2020-05-28 21:42:23 +0200

    3 files modified

--- src/app/shared/components/list/list.component.ts
+++ src/app/shared/components/list/list.component.ts
@@ -7,11 +7,28 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListComponent implements OnInit {
   @Input() items: any[];
-  @Input() label: string;
+  @Input() selection: any | any[];
+  @Input() label: string | ((any) => string);
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  private getLabel(item : any) : string {
+    if (typeof this.label === 'string') {
+      return item[this.label];
+    } else if (typeof this.label === 'function') {
+      return this.label(item);
+    }
+  }
+
+  private isActive(item : any) : boolean {
+    if (this.selection instanceof Array) {
+      return this.selection.indexOf(item) !== -1;
+    } else {
+      return this.selection === item;
+    }
   }
 
 }
